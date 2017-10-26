2a73164fbd54a8134c814ac0042e2a251685c2ae
Masatoshi Hayashi <literalice@monochromeroad.com>
2017-10-26 23:50:59 +0900

    add greeting

--- front/src/main/java/com/monochromeroad/springboot/demo/DemoApplication.java
+++ front/src/main/java/com/monochromeroad/springboot/demo/DemoApplication.java
@@ -2,20 +2,31 @@ package com.monochromeroad.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestOperations;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    static class DemoRestController {
+
+        private final RestOperations restOperations;
+
+        public DemoRestController(RestTemplateBuilder restTemplateBuilder) {
+            this.restOperations = restTemplateBuilder.build();
+        }
+
+        @RequestMapping("/")
+        String greeting() {
+            return restOperations.getForObject("http://greeting:8080/greeting", String.class);
+        }
     }
 }
 
